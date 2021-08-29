fe272de4706f42698c024c735fe7ca636ea2f0e6
Achraf Bardan <achrafbardan@gmail.com>
2021-08-29 10:51:03 +0200

    Forgot to add loaded event instead of navigating event to the modal

--- app/modal-model.ts
+++ app/modal-model.ts
@@ -1,6 +1,8 @@
 import { Color, EventData, Label, Observable } from '@nativescript/core'
 
 export class ModalModel extends Observable {
+  isBinded: boolean = true;
+
   labelLoadedModal (args: EventData) {
     const label = <Label>args.object;
 
